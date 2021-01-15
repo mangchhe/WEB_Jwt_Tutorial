9be80ecdc785c75d5d13bb4cd117097ae6a1eebe
HaJooHyunDesktop <mangchhe@naver.com>
2021-01-15 14:47:14 +0900

    의존성 설정

---
+++ src/test/java/me/hajoo/jwt/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hajoo.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
